e69a7cb5fce2b42958af80c1dc506b8f702e91e0
Khaliq <khaliqgant@gmail.com>
2026-03-31 11:14:10 +0200

    don't export sqlite

--- packages/server/src/storage/index.ts
+++ packages/server/src/storage/index.ts
@@ -1,3 +1,5 @@
 export * from "./interface.js";
 export * from "./compat.js";
-export * from "./sqlite.js";
+// sqlite storage is available via "@relayauth/server/storage/sqlite" subpath
+// export. Excluded from barrel to avoid bundling Function() dynamic import
+// in environments that disallow eval (e.g. Cloudflare Workers).
